d9bb0ae388422194750ccc5ab27fe6b1c270a111
Sirojiddin <sirojiddinyusupov55@gmail.com>
2024-05-23 10:30:18 +0500

    git fork

---
+++ script.js
@@ -0,0 +1,23 @@
+var name = prompt('Ismingizni kiriting')
+console.log(' Sizning isminiz ' + name);
+
+var age = +prompt('Yoshingizni kiriting')
+console.log(' Sizning yoshingiz ' + age);
+
+var num = +prompt (' Misolni yeching 25+10=? ')
+console.log(' 1 Misol 25+10=35 Sizning javobingiz ' + num);
+
+var num = +prompt (' Misolni yeching 123-23=? ')
+console.log(' 2 Misol 123-23=100 Sizning javobingiz ' + num);
+
+var num = +prompt (' Misolni yeching 100*10=? ')
+console.log( '3 Misol 100*10=1000 Sizning javobingiz ' + num );
+
+var num = +prompt (' Misolni yeching 24/2=? ')
+console.log( '4 Misol 24/2=12 Sizning javobingiz ' + num);
+
+var num = +prompt (' Misolni yeching 13%2=? ')
+console.log(' 5 Misol 13%2=1 Sizning javobingiz ' + num );
+
+var info = alert ( ' Natijani bilish uchun consolga kiring ' )
+
